51d5f9bdccc847e197b02c44ca84a2614ba086a6
Davíd Roggér <davidrogger.a@gmail.com>
2023-02-02 14:49:49 -0300

    STYLE: loading spinning effect

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render() {
     return (
-      <div className="loading-container">Loading</div>
+      <div className="loading-container"> </div>
     );
   }
 }
