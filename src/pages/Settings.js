aa04135200963407051939de5a52e00b9dab52dc
Davíd Roggér <davidrogger.a@gmail.com>
2023-02-02 10:48:53 -0300

    CHORE: created color pattern

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+// Estilos
+import '../styles/settings.css';
+
 class Settings extends Component {
   render() {
     return (
-      <div>
-        <h1 data-testid="settings-title">Settings</h1>
+      <div className="settings-container">
+        <div className="settings-forms">
+          <h1 data-testid="settings-title">Settings</h1>
+
+        </div>
       </div>
     );
   }
