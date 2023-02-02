2689503355b2e7e70b05122944e1c3762d2497a1
Davíd Roggér <davidrogger.a@gmail.com>
2023-02-02 15:11:15 -0300

    Updates

---
+++ precache-manifest.f8b589f52398e0d00ecbfb39dcdb1fc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d5393f37d4fbab8e6b336998c08129",
+    "url": "/trybe-project-trivia/index.html"
+  },
+  {
+    "revision": "7bd816c27ccb115190d7",
+    "url": "/trybe-project-trivia/static/css/main.4617fe6f.chunk.css"
+  },
+  {
+    "revision": "69a695c3b89bbc79ae75",
+    "url": "/trybe-project-trivia/static/js/2.dbbf42ea.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/trybe-project-trivia/static/js/2.dbbf42ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd816c27ccb115190d7",
+    "url": "/trybe-project-trivia/static/js/main.e5001ad5.chunk.js"
+  },
+  {
+    "revision": "3467b4a2b36b67901939",
+    "url": "/trybe-project-trivia/static/js/runtime-main.c9f40b0c.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trybe-project-trivia/static/media/trivia.466d153e.png"
+  }
+]);
